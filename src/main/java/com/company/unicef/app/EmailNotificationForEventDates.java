56ceaa52fe19827246f346a094b7f21f9c2738fa
Ilyas Raimkulov <31985607+TheIceEagle@users.noreply.github.com>
2024-02-03 20:53:45 +0600

    ilyas unicef_38_fixed_v2 Added minor fixes: 1)Added translations to event-edit.xml Event-date columns 2)Replaced in EmailNotificationForEventDates class sout to log.info using LoggerFactory

--- src/main/java/com/company/unicef/app/EmailNotificationForEventDates.java
+++ src/main/java/com/company/unicef/app/EmailNotificationForEventDates.java
@@ -4,11 +4,13 @@ import com.company.unicef.entity.Event;
 import com.company.unicef.entity.EventDate;
 import com.company.unicef.entity.EventUser;
 import com.company.unicef.entity.User;
-import groovy.util.logging.Slf4j;
+
 import io.jmix.core.DataManager;
 import io.jmix.core.FetchPlan;
 import io.jmix.core.security.Authenticated;
 import io.jmix.email.EmailException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.jmix.email.EmailInfo;
 import io.jmix.email.EmailInfoBuilder;
 import io.jmix.email.Emailer;
@@ -16,6 +18,8 @@ import org.checkerframework.checker.units.qual.A;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -30,11 +34,11 @@ import java.util.List;
 import java.util.UUID;
 
 
+
 import static java.util.stream.Collectors.*;
 
 
 @Component
-@Slf4j
 public class EmailNotificationForEventDates implements Job {
 
     @Autowired
@@ -42,6 +46,8 @@ public class EmailNotificationForEventDates implements Job {
 
     @Autowired
     private DataManager dataManager;
+
+    private static final Logger log = LoggerFactory.getLogger(EmailNotificationForEventDates.class);
 
     @Authenticated
     @Override
@@ -57,9 +63,9 @@ public class EmailNotificationForEventDates implements Job {
 
     public void sendNotificationByEmail() throws EmailException, IOException {
         LocalDateTime currentDate = LocalDateTime.now().truncatedTo(ChronoUnit.DAYS);
-        System.out.println("currentDate: " + currentDate);
+        log.info("currentDate: " + currentDate);
         LocalDateTime fourDaysAhead = currentDate.plusDays(3);
-        System.out.println("fourDaysAhead: " + fourDaysAhead);
+        log.info("fourDaysAhead: " + fourDaysAhead);
         int threeDaysBeforeMeeting = 3;
         int twoDaysBeforeMeeting = 2;
         int oneDayBeforeMeeting = 1;
@@ -72,16 +78,16 @@ public class EmailNotificationForEventDates implements Job {
                 .list();
 
         //print eventDates
-        System.out.println("eventDates: " + eventDates);
+        log.info("eventDates: " + eventDates);
         //check if eventDates is null or empty and check if eventDates are 24 hours, 48 hours or 72 hours before meeting and if they are send that meeting is in 24 hours, 48 hours or 72 hours
 
         if (eventDates == null || eventDates.isEmpty()) {
-            System.out.println("eventDates is null or empty");
+            log.info("eventDates is null or empty");
         } else {
             for (EventDate eventDate : eventDates) {
                 LocalDateTime roundedEventDate = eventDate.getEventDate();
                 long daysDifference = ChronoUnit.DAYS.between(currentDate, roundedEventDate);
-                System.out.println("daysDifference: " + daysDifference);
+                log.info("daysDifference: " + daysDifference);
                 //here check for threeDaysBeforeMeeting, twoDaysBeforeMeeting and oneDayBeforeMeeting
                 if (daysDifference == threeDaysBeforeMeeting) {
                     String threeDaysLeft = "3";
@@ -108,21 +114,21 @@ public class EmailNotificationForEventDates implements Job {
                 .fetchPlan(FetchPlan.BASE)
                 .one();
         //replace all sout to logging
-        System.out.println("event: " + event);
+        log.info("event: " + event);
 
         List<EventUser> eventUsers = dataManager.load(EventUser.class)
                 .query("select e from EventUser e where e.event.id = :eventId")
                 .parameter("eventId", event.getId())
                 .fetchPlan(FetchPlan.BASE)
                 .list();
-        System.out.println("eventUsers: " + eventUsers);
+        log.info("eventUsers: " + eventUsers);
 
         //get user emails from eventUsers based on user id
         List<UUID> userIds = eventUsers.stream()
                 .map(EventUser::getUser)
                 .map(User::getId)
                 .collect(toList());
-        System.out.println("userIds: " + userIds);
+        log.info("userIds: " + userIds);
 
         //get list of users  based of userIds through dataManager query
         List<User> users = dataManager.load(User.class)
@@ -130,35 +136,35 @@ public class EmailNotificationForEventDates implements Job {
                 .parameter("userIds", userIds)
                 .fetchPlan(FetchPlan.BASE)
                 .list();
-        System.out.println("users: " + users);
+        log.info("users: " + users);
 
         //get list of emails from users enitites
         List<String> emailList = users.stream()
                 .map(User::getEmail)
                 .collect(toList());
-        System.out.println("emailList: " + emailList);
+        log.info("emailList: " + emailList);
 
         //get emails from parents from event entity
         List<String> parentEmails = event.getParents().stream()
                 .map(com.company.unicef.entity.Parent::getEmail)
                 .collect(toList());
-        System.out.println("parentEmails: " + parentEmails);
+        log.info("parentEmails: " + parentEmails);
 
         //add parentEmails to emailList
         emailList.addAll(parentEmails);
-        System.out.println("emailList: " + emailList);
+        log.info("emailList: " + emailList);
 
 
         String message = daysDifference;
 
         for (String email : emailList) {
             if (email == null || email.isEmpty() || !email.contains("@")) {
-                System.out.println("email: " + email);
+                log.info("email: " + email);
                 emailList.remove(email);
                 // log about what happened
-                System.out.println("email: " + email + " was removed");
+                log.info("email: " + email + " was removed");
                 //log email that was removed
-                System.out.println("email: " + email + " was removed");
+                log.info("email: " + email + " was removed");
             }
         }
 
