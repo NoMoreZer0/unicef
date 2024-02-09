d3c37e78ddeb4beaccf6ea3156d5612d8de7c8b3
AbylaiNur <abylai.nurskenev@gmail.com>
2024-02-09 22:40:47 +0600

    - added table

---
+++ src/main/java/com/company/unicef/entity/StatisticsTableRow.java
@@ -0,0 +1,52 @@
+package com.company.unicef.entity;
+
+import io.jmix.core.entity.annotation.JmixGeneratedValue;
+import io.jmix.core.metamodel.annotation.InstanceName;
+import io.jmix.core.metamodel.annotation.JmixEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.UUID;
+
+@JmixEntity
+@Table(name = "STATISTICS_TABLE_ROW")
+@Entity
+public class StatisticsTableRow {
+    @JmixGeneratedValue
+    @Column(name = "ID", nullable = false)
+    @Id
+    private UUID id;
+
+    @InstanceName
+    @Column(name = "NAME")
+    private String name;
+
+    @Column(name = "COUNT_")
+    private Long count;
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
--- src/main/java/com/company/unicef/screen/statistics/StatisticsScreen.java
+++ src/main/java/com/company/unicef/screen/statistics/StatisticsScreen.java
@@ -1,12 +1,14 @@
 package com.company.unicef.screen.statistics;
 
 import com.company.unicef.entity.SchoolMask;
+import com.company.unicef.entity.StatisticsTableRow;
 import io.jmix.core.DataManager;
+import io.jmix.core.Messages;
 import io.jmix.ui.Notifications;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.component.ComboBox;
 import io.jmix.ui.component.DateField;
-import io.jmix.ui.component.Label;
+import io.jmix.ui.model.CollectionContainer;
 import io.jmix.ui.screen.Screen;
 import io.jmix.ui.screen.Subscribe;
 import io.jmix.ui.screen.UiController;
@@ -27,17 +29,11 @@ public class StatisticsScreen extends Screen {
     @Autowired
     private ComboBox<SchoolMask> schoolMaskComboBox;
     @Autowired
-    private Label secondFormCount;
-    @Autowired
-    private Label firstFormCount;
-    @Autowired
-    private Label openCaseCount;
-    @Autowired
-    private Label closedCaseCount;
-    @Autowired
-    private Label nullCaseCount;
-    @Autowired
     private Notifications notifications;
+    @Autowired
+    private CollectionContainer<StatisticsTableRow> statisticsTableRowDc;
+    @Autowired
+    private Messages messages;
 
     @Subscribe("calc")
     public void onCalcClick(final Button.ClickEvent event) {
@@ -76,6 +72,11 @@ public class StatisticsScreen extends Screen {
                 .parameter("dateBegin", dateBegin)
                 .parameter("dateEnd", dateEnd)
                 .one();
+        int individualPlanTotal = dataManager.loadValue("select count(e) from SecondForm e where e.schoolMask = :schoolMask and e.createdDate >= :dateBegin and e.createdDate < :dateEnd and e.openCase is not null", Integer.class)
+                .parameter("schoolMask", schoolMask)
+                .parameter("dateBegin", dateBegin)
+                .parameter("dateEnd", dateEnd)
+                .one();
         int openCaseTotal = dataManager.loadValue("select count(e) from OpenCase e where e.schoolMask = :schoolMask and e.openingDate >= :dateBegin and e.openingDate < :dateEnd and e.status = 'A'", Integer.class)
                 .parameter("schoolMask", schoolMask)
                 .parameter("dateBegin", dateBegin)
@@ -86,15 +87,23 @@ public class StatisticsScreen extends Screen {
                 .parameter("dateBegin", dateBegin)
                 .parameter("dateEnd", dateEnd)
                 .one();
-        int nullCaseTotal = dataManager.loadValue("select count(e) from OpenCase e where e.schoolMask = :schoolMask and e.openingDate >= :dateBegin and e.openingDate < :dateEnd and e.status is null", Integer.class)
-                .parameter("schoolMask", schoolMask)
-                .parameter("dateBegin", dateBegin)
-                .parameter("dateEnd", dateEnd)
-                .one();
-        secondFormCount.setValue(String.valueOf(secondFormsTotal));
-        firstFormCount.setValue(String.valueOf(firstFormsTotal));
-        openCaseCount.setValue(String.valueOf(openCaseTotal));
-        closedCaseCount.setValue(String.valueOf(closedCaseTotal));
-        nullCaseCount.setValue(String.valueOf(nullCaseTotal));
+        String firstFormText = messages.getMessage("com.company.unicef.screen.statistics/firstForm.caption");
+        String secondFormText = messages.getMessage("com.company.unicef.screen.statistics/secondForm.caption");
+        String individualPlanText = messages.getMessage("com.company.unicef.screen.statistics/individualPlan.caption");
+        String openCaseText = messages.getMessage("com.company.unicef.screen.statistics/openCase.caption");
+        String closedCaseText = messages.getMessage("com.company.unicef.screen.statistics/closedCase.caption");
+        statisticsTableRowDc.getMutableItems().clear();
+        statisticsTableRowDc.getMutableItems().add(createStatisticsTableRow(firstFormText, firstFormsTotal));
+        statisticsTableRowDc.getMutableItems().add(createStatisticsTableRow(secondFormText, secondFormsTotal));
+        statisticsTableRowDc.getMutableItems().add(createStatisticsTableRow(individualPlanText, individualPlanTotal));
+        statisticsTableRowDc.getMutableItems().add(createStatisticsTableRow(openCaseText, openCaseTotal));
+        statisticsTableRowDc.getMutableItems().add(createStatisticsTableRow(closedCaseText, closedCaseTotal));
+    }
+
+    private StatisticsTableRow createStatisticsTableRow(String name, long count) {
+        StatisticsTableRow statisticsTableRow = dataManager.create(StatisticsTableRow.class);
+        statisticsTableRow.setName(name);
+        statisticsTableRow.setCount(count);
+        return statisticsTableRow;
     }
 }
