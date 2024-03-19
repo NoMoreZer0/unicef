45f988aa1b14eec958c9b7fa1719626be6e97de3
NoMoreZer0 <kazakupersuper@mail.ru>
2024-03-19 17:16:37 +0500

    UNICEF 155: use immutable screen in event-edit

--- src/main/java/com/company/unicef/screen/event/EventEdit.java
+++ src/main/java/com/company/unicef/screen/event/EventEdit.java
@@ -1,18 +1,24 @@
 package com.company.unicef.screen.event;
 
 import com.company.unicef.entity.EventUser;
+import com.company.unicef.entity.Parent;
 import com.company.unicef.entity.User;
+import com.company.unicef.screen.parent.ParentBrowseImmutable;
 import com.company.unicef.screen.user.UserBrowse;
+import com.company.unicef.screen.user.UserBrowseImmutable;
 import io.jmix.core.DataManager;
 import io.jmix.core.Metadata;
 import io.jmix.ui.ScreenBuilders;
 import io.jmix.ui.action.Action;
+import io.jmix.ui.action.list.AddAction;
 import io.jmix.ui.component.Button;
 import io.jmix.ui.component.Table;
 import io.jmix.ui.model.*;
 import io.jmix.ui.screen.*;
 import com.company.unicef.entity.Event;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.inject.Named;
 
 @UiController("Event.edit")
 @UiDescriptor("event-edit.xml")
@@ -26,12 +32,14 @@ public class EventEdit extends StandardEditor<Event> {
     private CollectionPropertyContainer<EventUser> eventUsersDc;
     @Autowired
     private DataContext dataContext;
-
+    @Named("parentsTable.add")
+    private AddAction<Parent> parentsTableAdd;
+    
     @Subscribe("eventUsersTable.add")
     public void onEventUsersTableAdd(final Action.ActionPerformedEvent event) {
-        screenBuilders.lookup(eventUsersTable)
+        screenBuilders.lookup(User.class, this)
                 .withOpenMode(OpenMode.DIALOG)
-                .withScreenClass(UserBrowse.class)
+                .withScreenClass(UserBrowseImmutable.class)
                 .withSelectHandler(selectedUsers -> {
                     if (selectedUsers.isEmpty()) {
                         return;
